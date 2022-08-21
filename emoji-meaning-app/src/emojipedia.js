e295bc18d3eb1040d20042fb8ba0e200ab1d824f
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-08-21 13:37:43 +0530

    completed emojipedia app

---
+++ emoji-meaning-app/src/emojipedia.js
@@ -0,0 +1,55 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🫶",
+    name: "Heart Hands",
+    meaning:"Two hands forming a heart shape. Used to express love and support."
+  },
+  {
+    id: 5,
+    emoji: "🥹",
+    name: "Face Holding Back Tears",
+    meaning:"A yellow face with tears welling up at the bottom of its two large eyes. May be used to express a range of emotions including sadness, anger, embarrassment, admiration, and gratitude. "
+  },
+  {
+    id: 6,
+    emoji: "🥲 ",
+    name: "Smiling Face with Tear",
+    meaning:"A thin closed-mouth smile, and a single tear falling from one of its eyes. Can be used to express a wide range of emotions and expressions including gratitude,tender happiness, an attempt to look happy when actually sad, or smiling through pain."
+  },
+  {
+    id: 7,
+    emoji: "🥳 ",
+    name: "Partying Face",
+    meaning:"A yellow face with a party hat blowing a party horn as confetti floats around its head. Used for celebrating joyous occasions (such as New Year's Eve or World Emoji Day) and enjoying good times more generally."
+    },
+  {
+    id: 8,
+    emoji: "😉",
+    name: " Winking Face",
+    meaning:"A yellow face with a slight smile or open mouth shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."  }
+
+];
+
+export default emojipedia;
--- emoji-meaning-app/src/index.js
+++ emoji-meaning-app/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./components/App";
+import object from "./components/functions"
+import emojipedia from "./emojipedia"
+console.log(emojipedia);
+function createBox(eff){
+    return (
+    <object.Card
+    key={eff.id} emoji={eff.emoji} name={eff.name} meaning={eff.meaning}/>
+    );
+    
+}
+ReactDOM.render(
+    emojipedia.map(createBox)
+    ,document.getElementById("root"));
