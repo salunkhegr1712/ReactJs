1ade60ca3f6afd5d6a83d84a5526386fca029ad0
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-09-07 13:33:50 +0530

    the details of form and a code is added

---
+++ states/src/components/Form.jsx
@@ -0,0 +1,94 @@
+import React, { Component, useState } from "react";
+
+
+// in Component form we are using props 
+
+
+// now we will use the function Form to print changed every time when input is changed 
+function Form(props){
+    let name=""
+    let n1=""
+    let [stateVal,stateFunction]=useState(n1)
+
+    // so as we already created a Component so it is our part see which value is responsible for change 
+    function change(event){
+        // console.log("things in input were changed!")
+        // see wnat are the things which are changed as we change the content inside the event 
+
+        // console.log(event);
+        // ow we will see what went to chnage the value inside the block 
+        // console.log(event.target.value)
+        // console.log(event.target.type)
+        // console.log(event.target.placeholder)
+        name=event.target.value
+
+        // so now we are getting name so we have to rerender so now we will use state 
+        console.log(name)
+        // stateFunction(name)
+
+    }
+    // function which only get invoked if you click the mouse key on the button 
+    function changeName(){
+            console.log("clicked")
+            // n1=name
+            stateFunction(name)
+                // stateFunction(name)
+    }
+    return(
+        <div className="container">
+        <h1>{"Hello " + stateVal}</h1>
+
+        {/* now in input we are going to use the onchange event so that we can see how many time did the 
+        content inside the input get changed  */}
+        {/* on change will send call to the function and we can manupulate next steps inside the change function  */}
+        
+        <input 
+        type="text"
+        placeholder="Enter Your Name" 
+        onChange={change}
+        // so now we are able to fetch value to html also 
+        // so just make update value of tag in input like 
+        // value={name}
+        
+       />
+        <button onClick={changeName}> Submit</button>
+        </div>
+        
+
+    )
+}
+
+
+export default Form;
+
+// target block has so lot of attribute as there are large Number of factors are there from which change is occuring 
+
+// on inoputing value ghansham how things are get changed 
+
+// console.log(event.target.value)
+// console.log(event.target.type)
+// console.log(event.target.placeholder)
+// G
+// text
+// Enter Your Name
+// Gh
+// text
+// Enter Your Name
+// Gha
+// text
+// Enter Your Name
+// Ghan
+// text
+// Enter Your Name
+// Ghans
+// text
+// Enter Your Name
+// Ghansh
+// text
+// Enter Your Name
+// Ghansha
+// text
+// Enter Your Name
+// Ghansham
+// text
+// Enter Your Name
--- states/src/index.js
+++ states/src/index.js
@@ -1,12 +1,25 @@
 // importing basic function which we need in order to run this code 
-import React from "react";
-import Reactdom from "react-dom"
-import Login from "./components/Login"
+import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
+import React, { Component } from "react";
+import ReactDOM from "react-dom"
+import Form from "./components/Form"
 
 // so as a whole now as we know how can we rerender our page and grab some output exactly like that we can do the same 
 
 // render the page with component called Login 
-Reactdom.render(<Login/>,document.getElementById("root"))
+// Reactdom.render(<Login/>,document.getElementById("root"))
 
 // now we have see how can we add the event listeners to our code and how can we use them to make
 // our page interactive 
+
+
+// now we will use the forms inside the react and with use of the forms we will take some input from user and we 
+// will process it 
+
+// now we have to use firstly the events like the onclick onchange which get called every time if input Block 
+// have some things changed  
+//  we are passing thw value from abc and the props in the function block of Component is toHaveErrorMessage
+//  which is handling the input and throw it back also 
+ReactDOM.render(<Form abc="Hello"/>,document.getElementById("root"));
+
+
